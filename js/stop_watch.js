997ae0a3f1922c72ab3384e27cffcfecb31086f7
Bottleon <qncn93@gmail.com>
2022-05-10 20:53:31 +0900

    기록하기 기능 추가

--- js/stop_watch.js
+++ js/stop_watch.js
@@ -5,6 +5,13 @@ const STOP_WATCH_RESTART = document.getElementById("stop_watch_restart_btn");
 const STOP_WATCH_RESET = document.getElementById("stop_watch_reset_btn");
 const STOP_WATCH_PAUSE = document.getElementById("stop_watch_puase_btn");
 const STOP_WATCH_SUB_CLOCK = document.getElementById("stop_watch_sub_clock");
+const STOP_WATCH_RECORD = document.getElementById("stop_watch_record_btn");
+const STOP_WATCH_RECORD_LIST_BTN = document.getElementById(
+  "stop_watch_record_list_btn"
+);
+const STOP_WATCH_RECORD_LIST = document.getElementById(
+  "stop_watch_record_list"
+);
 
 /* define global variable */
 let stopWatchHours = 0;
@@ -17,16 +24,19 @@ STOP_WATCH_START.addEventListener("click", stopWatchStartFunc);
 STOP_WATCH_RESET.addEventListener("click", stopWatchResetFunc);
 STOP_WATCH_PAUSE.addEventListener("click", stopWatchPuaseFunc);
 STOP_WATCH_RESTART.addEventListener("click", stopWatchRestartFunc);
+STOP_WATCH_RECORD.addEventListener("click", recordStopWatchFunc);
+STOP_WATCH_RECORD_LIST_BTN.addEventListener("click", recordListFunc);
 
 /* define STOP_WATCH javascript Object */
 const STOP_WATCH = {
   clock: null,
+  record: [],
 };
 
 /* click start Button function */
 function stopWatchStartFunc() {
   STOP_WATCH_START.type = "hidden";
-  //STOP_WATCH_RESTART.type = "button";
+  STOP_WATCH_RECORD.type = "button";
   STOP_WATCH_PAUSE.type = "button";
   STOP_WATCH.clock = setInterval(startStopWatch, 10);
 }
@@ -53,14 +63,23 @@ function startStopWatch() {
 /* reset stopwatch */
 function stopWatchResetFunc() {
   clearInterval(STOP_WATCH.clock);
+
   stopWatchHours = 0;
   stopWatchMinutes = 0;
   stopWatchSeconds = 0;
   stopWatchCenti = 0;
+
   setTime();
+
   STOP_WATCH_PAUSE.type = "hidden";
   STOP_WATCH_RESTART.type = "hidden";
   STOP_WATCH_START.type = "button";
+  STOP_WATCH_RECORD.type = "hidden";
+
+  while (STOP_WATCH_RECORD_LIST.hasChildNodes()) {
+    STOP_WATCH_RECORD_LIST.removeChild(STOP_WATCH_RECORD_LIST.firstChild);
+  }
+  STOP_WATCH.record = [];
 }
 
 /** pause stopwatch */
@@ -74,6 +93,28 @@ function stopWatchPuaseFunc() {
 function stopWatchRestartFunc() {
   STOP_WATCH_RESTART.type = "hidden";
   stopWatchStartFunc();
+}
+
+/** record stopwatch */
+function recordStopWatchFunc() {
+  const record = `${
+    stopWatchHours < 10 ? `0${stopWatchHours}` : stopWatchHours
+  }:${stopWatchMinutes < 10 ? `0${stopWatchMinutes}` : stopWatchMinutes}:${
+    stopWatchSeconds < 10 ? `0${stopWatchSeconds}` : stopWatchSeconds
+  } ${stopWatchCenti < 10 ? `0${stopWatchCenti}` : stopWatchCenti}`;
+  console.log(record);
+  STOP_WATCH.record.push(record);
+}
+
+function recordListFunc() {
+  while (STOP_WATCH_RECORD_LIST.hasChildNodes()) {
+    STOP_WATCH_RECORD_LIST.removeChild(STOP_WATCH_RECORD_LIST.firstChild);
+  }
+  STOP_WATCH.record.forEach((ele) => {
+    const li = document.createElement("li");
+    li.innerHTML = ele;
+    STOP_WATCH_RECORD_LIST.appendChild(li);
+  });
 }
 
 /** time setting */
